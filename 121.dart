9aa8cfbd7682a36241e887adbf782deb7f6fea98
ilsong963 <ilsong963@naver.com>
2025-03-25 09:06:37 +0900

    121번 문제 해결

--- 121.dart
+++ 121.dart
@@ -4,14 +4,6 @@ class Solution {
 
     int min = prices[0];
 
-    // for (int i = 0; i < prices.length; i++) {
-    //   for (int j = i; j < prices.length; j++) {
-    //     if (prices[j] - prices[i] > profit) {
-    //       profit = prices[j] - prices[i];
-    //     }
-    //   }
-    // }
-
     for (int i = 0; i < prices.length; i++) {
       if (prices[i] < min) {
         min = prices[i];
@@ -26,5 +18,67 @@ class Solution {
 
 void main() {
   Solution s = Solution();
-  print(s.maxProfit([7, 1, 5, 3, 6, 4]));
+  print(
+    s.maxProfit([
+      138,
+      199,
+      111,
+      308,
+      126,
+      489,
+      667,
+      957,
+      278,
+      848,
+      629,
+      570,
+      368,
+      320,
+      413,
+      213,
+      835,
+      996,
+      225,
+      838,
+      826,
+      290,
+      970,
+      480,
+      64,
+      274,
+      756,
+      763,
+      928,
+      844,
+      32,
+      172,
+      828,
+      270,
+      763,
+      607,
+      907,
+      677,
+      544,
+      195,
+      699,
+      589,
+      677,
+      577,
+      847,
+      993,
+      290,
+      993,
+      495,
+      846,
+      887,
+      763,
+      627,
+      287,
+      534,
+      92,
+      965,
+      172,
+      423,
+    ]),
+  );
 }
