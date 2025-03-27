f292035cc7f4c61292a3ab2bc86920a19eef15af
ilsong963 <ilsong963@naver.com>
2025-03-27 16:34:11 +0900

    169번 문제 해결

---
+++ 169.dart
@@ -0,0 +1,29 @@
+class Solution {
+  int majorityElement(List<int> nums) {
+    int major = 0;
+    int answer = 0;
+
+    Map map = Map();
+
+    for (int i = 0; i < nums.length; i++) {
+      if (!map.containsKey(nums[i])) {
+        map.addAll({nums[i]: 1});
+      } else {
+        map.update(nums[i], (value) => map[nums[i]] + 1);
+      }
+    }
+
+    map.forEach((key, value) {
+      if (value > major) {
+        major = value;
+        answer = key;
+      }
+    });
+    return answer;
+  }
+}
+
+void main() {
+  Solution s = Solution();
+  print(s.majorityElement([2, 2, 1, 1, 1, 2, 2]));
+}
