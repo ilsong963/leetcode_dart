39e72a8adbf69f9845768e39009687082cb9f81c
ilsong963 <ilsong963@naver.com>
2025-04-21 09:16:39 +0900

    724번 문제 해결

---
+++ 724.dart
@@ -0,0 +1,30 @@
+class Solution {
+  int pivotIndex(List<int> nums) {
+    int total = 0;
+    int sum = 0;
+
+    for (int num in nums) {
+      total += num;
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      total -= nums[i];
+      print(nums[i]);
+
+      print("$sum == $total");
+      print('---------');
+
+      if (sum == total) {
+        return i;
+      }
+      sum += nums[i];
+    }
+    return -1;
+  }
+}
+
+void main() {
+  Solution s = Solution();
+
+  print(s.pivotIndex([1, 7, 3, 6, 5, 6]));
+}
